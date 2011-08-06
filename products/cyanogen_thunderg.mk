cdb848f6dbb5c9ca25e32fd1d1fdb8a78053f671
mik_os <kiril.mik.os@gmail.com>
2011-08-06 18:17:29 +0300

    update build props
    Change-Id: I697812e6e4613cd2eb00eddac58f57e8346f6170

--- products/cyanogen_thunderg.mk
+++ products/cyanogen_thunderg.mk
@@ -1,5 +1,3 @@
-$(call inherit-product, build/target/product/full_base.mk)
-
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
@@ -12,32 +10,37 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 # Include FM-Radio stuff (does not work at this moment)
 $(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
-PRODUCT_BRAND := LGE
+PRODUCT_BRAND := lge
 PRODUCT_DEVICE := thunderg
 PRODUCT_MODEL := LG-P500
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_NAME := cyanogen_thunderg
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=thunderg BUILD_UTC_DATE=0
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=thunderg BUILD_UTC_DATE=0 BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=LGE/thunderg/thunderg/thunderg:2.2.2/FRG83/eng.swapan.pati.20110222.163039:user/release-keys PRIVATE_BUILD_DESC="thunderg-user 2.2.2 FRG83 eng.swapan.pati.20110222.163039 release-keys"
 
 # Live wallpaper packages
 PRODUCT_PACKAGES += \
-        LiveWallpapers \
-        LiveWallpapersPicker \
-        MagicSmokeWallpapers \
-        VisualizationWallpapers
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
 
-#CYANOGEN_NIGHTLY := true
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/thunderg
 
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Thuderg-mik
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Thuderg
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-Thunderg
+            ro.modversion=CyanogenMod-7.1.0-RC1-Thunderg
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-Thunderg-mik
+            ro.modversion=CyanogenMod-7.1.0-RC1-Thunderg-KANG
     endif
 endif
 
